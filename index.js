562da36af1a7db49f178b168a9d714770c42ae6c
Azeem Contractor <azeem.contractor@gmail.com>
2022-12-19 23:01:51 -0500

    cors, static and public folder setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const videos = require('./routes/videos');
+const cors = require('cors');
 
 // Configuration
 require("dotenv").config();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 console.log("PORT");
 console.log(PORT)
+app.use(cors());
 // Middleware
 app.use((req, _res, next) => {
     console.log(`this is the middleware`);
@@ -16,11 +18,7 @@ app.use((req, _res, next) => {
 
 app.use('/videos', videos)
 
-app.use(express.static('public'));
-// // Home Route
-// app.get('/', (_request, response) => {
-//     response.send("Hello World");
-// });
+app.use(express.static('./public/images'));
 
 app.listen(PORT, () => {
     console.log(`API server listening on ${PORT}`);
